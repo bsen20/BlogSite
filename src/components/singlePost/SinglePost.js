23abc0ca88b81e728bcb34fbb5595104143ba239
bsen20 <officialbsen@gmail.com>
2021-09-28 23:06:49 +0530

    Writing posts editing and deleteing done successfully

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -1,12 +1,17 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import "./singlePost.css";
 const SinglePost = () => {
   const location = useLocation();
   const postId = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
+  const { user } = useContext(Context);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdateMode] = useState(false);
   useEffect(() => {
     const getPost = async () => {
       const post = await axios.get(`/posts/${postId}`);
@@ -22,18 +27,55 @@ const SinglePost = () => {
   if (post.photo) {
     imgUrl = post.photo;
   }
+  const handleDelete = async () => {
+    try {
+      await axios.delete("/posts/" + postId, {
+        data: { username: user.username },
+      });
+      window.location.replace("/");
+    } catch (error) {}
+  };
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`/posts/${post._id}`, {
+        username: user.username,
+        title,
+        desc,
+      });
+      window.location.reload();
+      setUpdateMode(false);
+    } catch (err) {}
+  };
 
   return (
     <div className="singlePost">
       <div className="singlePostContainer">
         <img src={imgUrl} alt="" className="singlePostImg" />
-        <h1 className="singlePostTitle">
-          {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
-        </h1>
+        {updateMode ? (
+          <input
+            type="text"
+            value={title}
+            className="singlePostTitleInput"
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className="singlePostTitle">
+            {post.title}
+            {post.username === user?.username && (
+              <div className="singlePostEdit">
+                <i
+                  className="singlePostIcon far fa-edit"
+                  onClick={() => setUpdateMode(true)}
+                ></i>
+                <i
+                  className="singlePostIcon far fa-trash-alt"
+                  onClick={handleDelete}
+                ></i>
+              </div>
+            )}
+          </h1>
+        )}
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
             Author :{" "}
@@ -45,7 +87,20 @@ const SinglePost = () => {
             {new Date(post.createdAt).toDateString()}{" "}
           </span>
         </div>
-        <p className="singlePostDesc">{post.desc}</p>
+        {updateMode ? (
+          <textarea
+            className="singlePostDescInput"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        ) : (
+          <p className="singlePostDesc">{post.desc}</p>
+        )}
+        {updateMode && (
+          <button className="singlePostButton" onClick={handleUpdate}>
+            Update
+          </button>
+        )}
       </div>
     </div>
   );
--- src/pages/write/Write.js
+++ src/pages/write/Write.js
@@ -1,24 +1,56 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { Context } from "../../context/Context";
 import "./write.css";
 const Write = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.photo = filename;
+    }
+    try {
+      const res = await axios.post("/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
+    } catch (error) {}
+  };
+  let imgUrl =
+    "https://jbtrainingsolutions.com/wp-content/uploads/2020/01/Valley-Taurus-Mountains-Turkey.jpg";
+  if (file) {
+    imgUrl = URL.createObjectURL(file);
+  }
   return (
     <div className="write">
-      <img
-        src="https://jbtrainingsolutions.com/wp-content/uploads/2020/01/Valley-Taurus-Mountains-Turkey.jpg"
-        alt=""
-        className="writeImg"
-      />
-      <form className="writeForm">
+      <img src={imgUrl} alt="" className="writeImg" />
+      <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
             <i className="writeIcon fas fa-plus"></i>
           </label>
-          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <input
             type="text"
             placeholder="Title"
             autoFocus={true}
             className="writeInput"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="writeFormGroup">
@@ -26,9 +58,12 @@ const Write = () => {
             className="writeInput writeText"
             placeholder="Tell your story"
             type="text"
+            onChange={(e) => setDesc(e.target.value)}
           ></textarea>
         </div>
-        <button className="writeSubmit">Publish</button>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
       </form>
     </div>
   );
